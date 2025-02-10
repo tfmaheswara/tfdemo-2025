6f71b68268af8b0e11dfb5dcf325dd0c67b215d6
tfmaheswara <158280548+tfmaheswara@users.noreply.github.com>
2025-02-10 07:05:55 +0000

    1st commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Change based on your AWS region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
