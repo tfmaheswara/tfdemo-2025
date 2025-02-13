37b4b5c3ecf107341e8d6387fc538d9b77669c48
CSG <brcsgint@broadridge.com>
2025-02-13 14:46:45 +0530

    2nd

--- varables.tf
+++ varables.tf
@@ -2,5 +2,5 @@ variable "region" {
     default = "us-east-1"
 }
 variable "security_group_id" {
-  default = ["sg-0043de2da52bdff41", "sg-07905bd9a7b33776e"]
+  default = "sg-0043de2da52bdff41" #["sg-0043de2da52bdff41", "sg-07905bd9a7b33776e"]
 }
