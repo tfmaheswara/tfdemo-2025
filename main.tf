d452371a6358cb29b0825b1c77c4529bf88f96b4
CSG <brcsgint@broadridge.com>
2025-02-13 15:17:18 +0530

    creating an ec2 instance

--- main.tf
+++ main.tf
@@ -1,34 +1,32 @@
 provider "aws" {
   region = var.region
-  
 }
 
-/*
-data "aws_ami" "ubuntu_ami" {
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
   filter {
-    name   = "ubuntu-eks-pro/k8s_1.30/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240830"
-    values = ["ami-0004facbfb2f47dd8"]
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-*/
 
 resource "aws_instance" "web" {
-  ami           = "ami-0000791bad666add5"
+  ami           = data.aws_ami.latest_amazon_linux.id #data.aws_ami.latest_amazon_linux.id  #"ami-0000791bad666add5"
   instance_type = "t2.micro"
   #instance_type = var.typeofinstace
   subnet_id     = "subnet-01b2e6905e667ab81"
   security_groups = ["sg-0043de2da52bdff41", "sg-07905bd9a7b33776e"]
-  tags = {
+  tags = {s
     Name = "HelloWorld"
   }
   #tags = local.common_tags
 }
 
-
 output "instance_ip" {
   value = aws_instance.web.public_ip
 }
-
 
 locals {
   keyname1 = "testdemo"
--- varables.tf
+++ varables.tf
@@ -1,6 +1,7 @@
 variable "region" {
     default = "us-east-1"
 }
+
 variable "security_group_id" {
   default = "sg-0043de2da52bdff41" #["sg-0043de2da52bdff41", "sg-07905bd9a7b33776e"]
 }
